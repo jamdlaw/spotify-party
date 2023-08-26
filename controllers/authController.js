bd1a3d7040457121a4837888e7d5e42535c576ec
James Lawrence <jamdlaw@yahoo.com>
2023-08-26 12:07:00 -0700

    added controllers folder and created authService file

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const authService = require('../services/authService');
+
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+  const authURL = authService.generateAuthURL();
+  res.redirect(authURL);
+});
+
+router.get('/callback', async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const tokens = await authService.exchangeCodeForTokens(code);
+    // Save tokens to user session or database
+    res.send('Authentication successful!');
+  } catch (error) {
+    console.error('Error during authentication:', error);
+    res.status(500).send('Authentication failed');
+  }
+});
+
+module.exports = router;
