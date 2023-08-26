4bc0281e7672badb564ba2095a78b8cca1e8fe18
James Lawrence <jamdlaw@yahoo.com>
2023-08-26 12:41:00 -0700

    updates to package and index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const spotifyController = require('../controllers/spotifyController');
+const playlistController = require('../controllers/playlistController');
 
 const router = express.Router();
 
+// Authentication routes
 router.get('/login', authController.login);
 router.get('/callback', authController.callback);
 
+// Spotify-related routes
 router.get('/listen-history', spotifyController.getListenHistory);
 router.get('/create-playlist', spotifyController.createPlaylist);
 router.get('/add-songs', spotifyController.addSongsToPlaylist);
+
+// Playlist-related routes
+router.post('/create-playlist', playlistController.createPlaylist);
+router.post('/add-songs', playlistController.addSongsToPlaylist);
 
 module.exports = router;
