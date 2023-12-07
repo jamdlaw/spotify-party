f7c3276d2473d026f64b107c50f5fcf2debf4e41
James Lawrence <jamdlaw@yahoo.com>
2023-12-07 14:43:32 -0800

    added insert track data functions

---
+++ insertTrackData.js
@@ -0,0 +1,57 @@
+const mysql = require('./utils/mysqlUtils');
+
+/*
+// Sample data object
+const sampleData = {
+  "items": [
+    // Your sample data here...
+  ]
+};
+*/
+
+// Function to insert data into the tables
+export default async function insertDataIntoTables(sampleData)   {
+  for (const item of sampleData.items) {
+    // Insert artists
+    const artist = item.track.artists[0];
+    await mysql.promise().query('INSERT IGNORE INTO Artists (artist_id, artist_name, genres, popularity) VALUES (?, ?, ?, ?)', [
+      artist.id,
+      artist.name,
+      artist.genres.join(', '), // Assuming genres is an array
+      artist.popularity
+    ]);
+
+    // Insert albums
+    const album = item.track.album;
+    await mysql.promise().query('INSERT IGNORE INTO Albums (album_id, album_name, album_type, total_tracks, release_date) VALUES (?, ?, ?, ?, ?)', [
+      album.id,
+      album.name,
+      album.album_type,
+      album.total_tracks,
+      album.release_date
+    ]);
+
+    // Insert tracks
+    await mysql.promise().query('INSERT INTO Tracks (track_id, album_id, track_name, album_name, album_type, total_tracks, release_date, track_number, duration_ms, explicit, popularity, preview_url, is_local, played_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+      item.track.id,
+      album.id,
+      item.track.name,
+      album.name,
+      album.album_type,
+      album.total_tracks,
+      album.release_date,
+      item.track.track_number,
+      item.track.duration_ms,
+      item.track.explicit,
+      item.track.popularity,
+      item.track.preview_url,
+      item.track.is_local,
+      item.played_at
+    ]);
+  }
+
+  console.log('Data inserted successfully.');
+};
+
+
+ 
