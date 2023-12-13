676d12e68cbee99ef1acf7c6083f1db3e2b0c21c
James Lawrence <jamdlaw@yahoo.com>
2023-12-13 13:55:36 -0800

    added begining of sql function

--- createPlaylist.js
+++ createPlaylist.js
@@ -1,7 +1,10 @@
 const mysql = require('./utils/mysqlUtils');
 
 const createPlaylist = (accessToken) => {
-    console.log('made it to function');     
+    sql = "SELECT track_id FROM recommended_tracks limit 5;";
+    mysql.query(sql).then(tracks =>{
+        console.log(tracks);
+    })
 };
 
 module.exports = createPlaylist;
