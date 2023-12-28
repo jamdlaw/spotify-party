f6c0e7d8653512fdad49b3fd9373b83d6056d900
James Lawrence <jamdlaw@yahoo.com>
2023-12-28 15:32:42 -0800

    fixed my id return issue

--- createParty.js
+++ createParty.js
@@ -1,18 +1,12 @@
 const mysql = require('./utils/mysqlUtils');
 
-const createParty = (userid, partyname) => {
+const createParty = async (userid, partyname) => {
   const sqlQuery = 'INSERT INTO party ( party_name, user_id ) VALUES(?,?)';
-    let results = '';
-    try{
-      mysql.query(sqlQuery,[partyname , userid]).then((results) => {
-        //console.log(results.insertId);
-        return results.insertId;
-      }
-        
-      );
-    } catch(error){
-      console.log(error);
-    }
+  let results = '';
+  results = await mysql.query(sqlQuery,[partyname , userid]);
+  console.log('create party ' + results.insertId);
+  return results.insertId;
+  
 }
 
 module.exports = createParty;
