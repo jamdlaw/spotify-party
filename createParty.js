c8341b95ea452edf2b32fdf7dc4c9586519099a1
James Lawrence <jamdlaw@yahoo.com>
2023-12-29 15:33:37 -0800

    correct case on veriable names

--- createParty.js
+++ createParty.js
@@ -1,10 +1,10 @@
 const mysql = require('./utils/mysqlUtils');
 
-const createParty = async (userid, partyname) => {
+const createParty = async (userId, partyName) => {
   const sqlQuery = 'INSERT INTO party ( party_name, user_id ) VALUES(?,?)';
   let results = '';
   try{
-    results = await mysql.query(sqlQuery,[partyname , userid]);
+    results = await mysql.query(sqlQuery,[partyName , userId]);
   }catch(error){
     console.log(error);
   }
