234b296c299588e5512bc948a44fa901004d9f96
James Lawrence <jamdlaw@yahoo.com>
2023-12-29 13:24:44 -0800

    added back try catch

--- createParty.js
+++ createParty.js
@@ -3,10 +3,14 @@ const mysql = require('./utils/mysqlUtils');
 const createParty = async (userid, partyname) => {
   const sqlQuery = 'INSERT INTO party ( party_name, user_id ) VALUES(?,?)';
   let results = '';
-  results = await mysql.query(sqlQuery,[partyname , userid]);
-  console.log('create party ' + results.insertId);
-  return results.insertId;
+  try{
+    results = await mysql.query(sqlQuery,[partyname , userid]);
+    console.log('create party ' + results.insertId); 
+  }catch(error){
+    console.log(error);
+  }
   
+  return results.insertId;
 }
 
 module.exports = createParty;
