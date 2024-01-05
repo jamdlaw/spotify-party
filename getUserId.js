f15ecebc6c207d092d6dbb108dbd6a7ad0ad8ed3
James Lawrence <jamdlaw@yahoo.com>
2024-01-04 21:06:48 -0800

    fixed return value

--- getUserId.js
+++ getUserId.js
@@ -2,15 +2,14 @@ const mysql = require('./utils/mysqlUtils');
 
     const getUserId = async (email) =>{
         const sql = "SELECT id FROM USERS WHERE email = ? ";
-        
         let results = '';
         try{  
             results = await mysql.query(sql, email);
         } catch(error){
             console.log(error);
         }
-        console.log(results);
-        return results.id;
+        
+        return results[0];
     }
 
 module.exports = getUserId;
