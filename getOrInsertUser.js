5719144e8018e27fefc00348cb1ebb7c75a0b56b
James Lawrence <jamdlaw@yahoo.com>
2024-01-07 12:14:10 -0800

    working

---
+++ getOrInsertUser.js
@@ -0,0 +1,11 @@
+const mysql = require('./utils/mysqlUtils');
+
+const getOrInsertUser = (email, name) => {
+//check if user exists? 
+// yes : return userID
+
+//no
+//insert new user
+}
+
+module.exports = getOrInsertUser;
