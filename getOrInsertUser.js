cdc9e0b2197c69c61bf977c095a3ad37558917c5
James Lawrence <jamdlaw@yahoo.com>
2024-01-07 12:19:21 -0800

    added function

--- getOrInsertUser.js
+++ getOrInsertUser.js
@@ -1,11 +1,18 @@
 const mysql = require('./utils/mysqlUtils');
+const getUserId = require('./getUserId');
 
 const getOrInsertUser = (email, name) => {
 //check if user exists? 
 // yes : return userID
-
+let userId = getUserId(email);
+if(userId){
+    return userId;
+}
 //no
 //insert new user
+userId = createUser(name , email);
+
+return userId; 
 }
 
 module.exports = getOrInsertUser;
