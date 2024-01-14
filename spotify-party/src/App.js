b8c1650812c79a93afc260ed7c0bffe7f8fd0b16
James Lawrence <jamdlaw@yahoo.com>
2024-01-14 11:25:26 -0800

    added logo

--- spotify-party/src/App.js
+++ spotify-party/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import logo from './spotify_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>Join the party!</h1>
-          
+        <img src={logo} className="App-logo" alt="logo" />
+        <form action="" name="partyform" id="partyform" method="post">
+            <input type="text" name="party_name" id="party_name" />
+            <input type="submit" class="btn btn-primary" value="Create a party" />
+          </form>
+        <a href="/joinParty" class="btn btn-primary">Join a party</a>
     </div>
   );
 }
