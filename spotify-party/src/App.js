1e87081c37e106405839d57834f762ff1cbb042b
James Lawrence <jamdlaw@yahoo.com>
2024-01-15 10:38:31 -0800

    made some changes to markup

--- spotify-party/src/App.js
+++ spotify-party/src/App.js
@@ -2,15 +2,25 @@ import logo from './spotify_logo.png';
 import './App.css';
 
 function App() {
+
+  function createParty(formData){
+    formData.preventDefault();
+    console.log('Form Data:', formData);
+    // You can perform other actions with the form data here
+    //const query = partyform.get("party_name");
+    //alert(`Party name is: '${query}'`);
+  }
+
   return (
     <div className="App">
         <h1>Join the party!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <form action="" name="partyform" id="partyform" method="post">
+        <form action={createParty} name="partyform" id="partyform" method="post">
             <input type="text" name="party_name" id="party_name" />
-            <input type="submit" class="btn btn-primary" value="Create a party" />
-          </form>
-        <a href="/joinParty" class="btn btn-primary">Join a party</a>
+            <input type="submit" className="btn btn-primary" value="Create a party" />
+        </form>
+        <h3> OR </h3>
+        <a href="/joinParty" className="btn btn-primary">Join a party</a>
     </div>
   );
 }
