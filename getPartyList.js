dd5bd9419c82985cca7a5a962a1204e2a7165ade
James Lawrence <jamdlaw@yahoo.com>
2024-01-16 15:02:34 -0800

    this code should have already been pushed

--- getPartyList.js
+++ getPartyList.js
@@ -1,15 +1,16 @@
 const mysql = require('./utils/mysqlUtils');
 
     const getPartyList = async () =>{
-        const sql = "SELECT id, party_name FROM party;";
-        let results = '';
-        try{  
-            results = await mysql.query(sql);
-        } catch(error){
-            console.log(error);
-        }
-        
-        return results;
+        return new Promise(async (resolve, reject) => {
+            const sql = "SELECT id, party_name FROM party;";
+            let results = [];
+            try{  
+                results = await mysql.query(sql);
+                resolve(results);
+            } catch(error){
+                resolve(results);
+            }
+        });
     }
 
 module.exports = getPartyList;
