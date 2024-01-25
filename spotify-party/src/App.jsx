b1d10f8fa6470717439f09f0d81d0726aa88acb6
James Lawrence <jamdlaw@yahoo.com>
2024-01-25 11:37:16 -0800

    added button component

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   
+  //<a href="http://localhost:8888/login" className="btn btn-primary">Log in to Spotify</a>
   return (
     <>
       <div>
-        <h1> Spotify Party Demo</h1>
-        <a href="http://localhost:8888/login" className="btn btn-primary">Log in to Spotify</a>
+        <h1> Spotify Party Demo</h1>      
+        <Button />
       </div> 
     </>
   )
