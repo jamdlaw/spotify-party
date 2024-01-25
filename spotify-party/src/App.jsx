062f9215dcb8f4fcef6ebdded74f29092e2e7ff0
James Lawrence <jamdlaw@yahoo.com>
2024-01-25 11:47:04 -0800

    added button to page and some defult code

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -7,11 +7,14 @@ import Button from './components/Button'
 function App() {
   
   //<a href="http://localhost:8888/login" className="btn btn-primary">Log in to Spotify</a>
+
+  const [alertVisable, setAlertVisablilty ] = useState(false);
+
   return (
     <>
       <div>
         <h1> Spotify Party Demo</h1>      
-        <Button />
+        <Button color='primary' onClick={() => setAlertVisablilty(true)}> My text </Button>
       </div> 
     </>
   )
