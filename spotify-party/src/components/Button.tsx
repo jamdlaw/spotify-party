c39a970edcf9f67d15d3cdd1186eee2b79d38cd9
James Lawrence <jamdlaw@yahoo.com>
2024-01-25 11:46:29 -0800

    changed button to typescript type

---
+++ spotify-party/src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface Props {
+    children: string;
+    onClick: () => void;
+    color?: 'primary' | 'secoundary' | 'success';
+}
+
+const Button = ({children, onClick, color ="primary"}: Props) => {
+  return (
+    <button 
+        type="button" 
+        className={'btn btn-' + color}
+        onClick={onClick}
+    >
+            {children}
+    </button>
+    
+  )
+}
+
+export default Button
