38a47c4619c19bb2ad17a8424be7e94a56d6efaa
James Lawrence <jamdlaw@yahoo.com>
2024-01-31 13:28:15 -0800

    made login code into login component

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -1,23 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Login'
 import Button from './components/Button'
 
 function App() {
   
-  const Login = () => {
-    fetch('http://localhost:8888/testUserId')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-  }
-
+ 
   return (
-    <>
-      <div>
-        <h1> Spotify Party Demo</h1>      
-        <Button color='primary' onClick={() => Login()}> Log In </Button>
-      </div> 
-    </>
+    <Login />
   )
 }
 
--- spotify-party/src/components/Login.tsx
+++ spotify-party/src/components/Login.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Button from './Button';
 
 function Login() {
+  
+    const Login = () => {
+        fetch('http://localhost:8888/testUserId')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+        }
+    
   return (
-    <div>Login</div>
+    <>
+      <div>
+        <h1> Spotify Party Demo</h1>      
+        <Button color='primary' onClick={() => Login()}> Log In </Button>
+      </div> 
+    </>
   )
 }
 
