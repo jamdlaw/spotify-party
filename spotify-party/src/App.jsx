2e0cbf64f06837d53ab5bb74cade43011fcb53ae
James Lawrence <jamdlaw@yahoo.com>
2024-01-31 13:35:23 -0800

    added new form to app

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './components/Login'
-import Button from './components/Button'
+import CreateOrJoinParty from './components/CreateOrJoinParty'
 
 function App() {
   
  
   return (
+    <>
     <Login />
+    <CreateOrJoinParty />
+    </>
   )
 }
 
