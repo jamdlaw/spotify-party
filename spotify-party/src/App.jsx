2b5ac09489e716d86c4d534b0bb73c29d4c124d1
James Lawrence <jamdlaw@yahoo.com>
2024-02-07 11:57:31 -0800

    added footer to router

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 import Login from './components/Login'
 import CreateOrJoinParty from './components/CreateOrJoinParty'
 import CreateParty from './CreateParty'
+import Footer from './Footer';
 
 function App() {
   
- 
   return (
     <BrowserRouter>
       <Routes>
@@ -15,6 +15,7 @@ function App() {
       <Route path="/CreateOrJoinParty" element={<CreateOrJoinParty/>}></Route>
       <Route path="/CreateParty" element={<CreateParty/>}></Route>
       </Routes>  
+      <Footer></Footer>
     </BrowserRouter>
   )
 }
