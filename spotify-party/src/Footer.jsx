fcdcfbebaf08b579ee062b6425d90b7311d8f97d
James Lawrence <jamdlaw@yahoo.com>
2024-02-07 11:56:53 -0800

    added footer

---
+++ spotify-party/src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-body-tertiary text-center text-lg-start">
+                <div className="text-center p-3">
+                    © 2020 Copyright:
+                    <a className="text-body" href="http://spotify-party.com/">spotify-party.com</a>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
