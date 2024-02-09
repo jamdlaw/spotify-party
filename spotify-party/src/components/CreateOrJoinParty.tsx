e50cdb101e098944b98f40cc2a16cf2389a50bbb
James Lawrence <jamdlaw@yahoo.com>
2024-02-09 11:33:57 -0800

    added button click for join party component

--- spotify-party/src/components/CreateOrJoinParty.tsx
+++ spotify-party/src/components/CreateOrJoinParty.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 import Button from './Button'
 import CreateParty from '../CreateParty';
+import JoinParty from '../JoinParty';
 
 function CreateOrJoinParty(props) {
 
     const [showCreateParty, setshowCreateParty] = useState(false);
+    const [showJoinParty, setshowJoinParty] = useState(false);
   
     const createParty = () => {
       setshowCreateParty(true);
+      setshowJoinParty(false);
     }
 
     const joinParty = () => {
        setshowCreateParty(false);
-       
+       setshowJoinParty(true);
+
     }    
 
   return (
@@ -20,6 +24,7 @@ function CreateOrJoinParty(props) {
         <h3>Create of join a party</h3>
         <Button onClick={createParty}>Create Party</Button> | <Button onClick={joinParty}>Join Party</Button>
         {showCreateParty && <CreateParty />}
+        {showJoinParty && <JoinParty />}
     </>
   )
 }
