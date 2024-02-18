99d1772bbf2ac1077691262e19dd277baa2c0fd7
James Lawrence <jamdlaw@yahoo.com>
2024-02-17 18:15:34 -0800

    added clickable list

---
+++ spotify-party/src/components/ClickableList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap is imported
+
+const ClickableList = ({ parties }) => {
+  // Function to handle click events on list items
+  const handleClick = (party) => {
+    alert(`Clicked on party: ${party.party_name || 'No name'}`);
+  };
+
+  return (
+    <div className="container mt-3">
+      <ul className="list-group">
+        {parties.map((party) => (
+          <li
+            key={party.id}
+            className="list-group-item list-group-item-action"
+            onClick={() => handleClick(party)}
+          >
+            {party.party_name || 'No name'} {/* Display 'No name' if party_name is null */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ClickableList;
