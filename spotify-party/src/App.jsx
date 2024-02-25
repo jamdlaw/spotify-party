93f81bf3c0ad0232470bd1b49b6daa2afa689edf
James Lawrence <jamdlaw@yahoo.com>
2024-02-25 12:08:41 -0800

    removed react version of login from app

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Login from './components/Login'
 import CreateOrJoinParty from './components/CreateOrJoinParty'
 import CreateParty from './components/CreateParty'
 import Footer from './components/Footer';
@@ -11,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Login/>}></Route>
+      <Route path="/" element={<CreateOrJoinParty/>}></Route>
       <Route path="/CreateOrJoinParty" element={<CreateOrJoinParty/>}></Route>
       <Route path="/CreateParty" element={<CreateParty/>}></Route>
       </Routes>  
