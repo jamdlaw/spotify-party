a4543f99a2a1a988e85da14438ecc341ba50e317
James Lawrence <jamdlaw@yahoo.com>
2024-02-25 13:54:29 -0800

    added async await for sql insert results

--- joinGuestToParty.js
+++ joinGuestToParty.js
@@ -1,14 +1,14 @@
 const mysql = require('./utils/mysqlUtils');
 
-const joinGuestToParty = (userId, partyId, is_host = 0) => {
+const joinGuestToParty = async (userId, partyId, is_host = 0) => {
     const sql = 'INSERT INTO party_guests(user_id, party_id,is_host) VALUES (?,?,?);';
     try{  
-        results = mysql.query(sql, [userId, partyId, is_host]);
+        results = await mysql.query(sql, [userId, partyId, is_host]);
     } catch(error){
         console.log(error);
     }
-
-    return results;
+    
+    return results.insertId;
       
 }
 
