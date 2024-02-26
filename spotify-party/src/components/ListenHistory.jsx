3e220d6e46d5b3acfd5bc4ee87542e676c728628
James Lawrence <jamdlaw@yahoo.com>
2024-02-26 12:30:21 -0800

    now sending request to back end

--- spotify-party/src/components/ListenHistory.jsx
+++ spotify-party/src/components/ListenHistory.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 const getListenHistory = () => {
   function getListenHistory(){
     const access_token = Cookies.get('access_token');
-    fetch(`/history?access_token=${encodeURIComponent(access_token)}`)
+    fetch(`http://localhost:8888/history?access_token=${encodeURIComponent(access_token)}`)
     .then(response => response.json()) // assuming the server response is JSON
     .then(data => {
       console.log(data);
