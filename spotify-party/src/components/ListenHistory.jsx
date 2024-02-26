fdda3d7abdd53116df05093650197e0c2aace442
James Lawrence <jamdlaw@yahoo.com>
2024-02-26 10:43:57 -0800

    added code for making request to get listen history

--- spotify-party/src/components/ListenHistory.jsx
+++ spotify-party/src/components/ListenHistory.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Cookies from 'js-cookie';
 
 const getListenHistory = () => {
+  function getListenHistory(){
+    const access_token = Cookies.get('access_token');
+    fetch(`/history?access_token=${encodeURIComponent(access_token)}`)
+    .then(response => response.json()) // assuming the server response is JSON
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+  }
+
   return (
+    <>
     <div>getListenHistory</div>
+    <Button onClick={getListenHistory}>Create Party</Button> 
+    </>   
   )
 }
 
