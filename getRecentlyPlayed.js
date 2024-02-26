7b41942fe7a6df08f7b190c091071913f06edef3
James Lawrence <jamdlaw@yahoo.com>
2024-02-26 13:20:21 -0800

    refectored function readalibty and testing

--- getRecentlyPlayed.js
+++ getRecentlyPlayed.js
@@ -1,18 +1,25 @@
-
-const getRecentlyPlayed = async accessToken => {
+const getRecentlyPlayed = async (accessToken) => {
   const url = 'https://api.spotify.com/v1/me/player/recently-played?limit=10';
 
-  return fetch(url, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-    .then(res => res.json())
-    .then(data => data.items)
-    .catch(error => console.log(error));
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data.items;
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 module.exports = getRecentlyPlayed;
