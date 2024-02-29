4fe39db3a28b4c4f56f95b7588bf2c6613aa3f76
James Lawrence <jamdlaw@yahoo.com>
2024-02-29 13:36:39 -0800

    added check to make sure listenHistory is set to avoid error

--- spotify-party/src/components/ListenHistoryResults.jsx
+++ spotify-party/src/components/ListenHistoryResults.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const ListenHistoryResults = ({ listenHistory }) => {
+  // Check if listenHistory is not set or is empty
+  if (!listenHistory || Object.keys(listenHistory).length === 0) {
+    // You can return null or any placeholder to indicate no content
+    return <div>No listen history available.</div>;
+  }
+  
   const historyItems = Object.values(listenHistory);  
   return (
     <div>
