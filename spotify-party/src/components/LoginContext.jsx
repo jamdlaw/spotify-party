82e971b0322269662188f49c9b1be5b7e905dbf1
James Lawrence <jamdlaw@yahoo.com>
2024-02-29 13:43:11 -0800

    disabled react login for now

--- spotify-party/src/components/LoginContext.jsx
+++ spotify-party/src/components/LoginContext.jsx
@@ -9,13 +9,13 @@ export function useLogin() {
 }
 
 export const LoginProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true); //2 disable ract login login = true
 
   // You would have a method to check if user is logged in
   // For example, checking a token in localStorage
   const checkLogin = () => {
     const access_token = Cookies.get('access_token');
-    setIsLoggedIn(access_token);
+    setIsLoggedIn(true); //2 disable ract login login = true
   };
 
   const value = {
