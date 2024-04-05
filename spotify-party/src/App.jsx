6b04c48e799e0861378e22b0981bed72281ef1b3
James Lawrence <jamdlaw@yahoo.com>
2024-04-05 10:44:21 -0700

    added nav bar to pages

--- spotify-party/src/App.jsx
+++ spotify-party/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <LoginProvider>
       <BrowserRouter>
+        <Navbar/>
         <Routes>
         <Route path="/" element={<CreateOrJoinParty/>}></Route>
         <Route path="/CreateOrJoinParty" element={<CreateOrJoinParty/>}></Route>
